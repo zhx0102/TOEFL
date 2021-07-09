19c66823b3b33e88b374f4fc4e22485675ac6ad2
zhenghuixian0102 <zhenghuixian0102@163.com>
2021-07-09 10:27:36 +0800

    all0709

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,3 +1,37 @@
 <template>
-  
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <Header />
+        <List />
+        <Footer />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import List from "./components/List.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    List,
+    Footer,
+  },
+};
+</script>
+
+<style>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
