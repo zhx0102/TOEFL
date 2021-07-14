90f648639bc77d197b84ecb620df2dce191c685d
zhenghuixian0102 <zhenghuixian0102@163.com>
2021-07-14 16:26:11 +0800

    all0714

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,21 +1,3 @@
 <template>
-<div id="app">
-  <div class="container">
-   <Header/>
-   <Main/>
-  </div>
-</div>
+  
 </template>
-<script>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-export default {
-    name:"App",
-    components:{
-        Header,
-        Main
-    }
-}
-</script>
-<style  scoped>
-</style>
